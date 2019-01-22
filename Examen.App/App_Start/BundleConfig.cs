c9e0699470a97fb9569e50d82357167136bb6868
Vallin Plasencia <vallin.plasencia@gmail.com>
2019-01-22 15:33:05 -0500

    Integrando el Frontend al Backend

--- Examen.App/App_Start/BundleConfig.cs
+++ Examen.App/App_Start/BundleConfig.cs
@@ -22,6 +22,15 @@ namespace Examen.App
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                      "~/Scripts/runtime*",
+                      "~/Scripts/polyfills*",
+                      "~/Scripts/main*",
+                      "~/Scripts/1*",
+                      "~/Scripts/5*",
+                      "~/Scripts/6*"));
         }
     }
 }
--- Examen.App/App_Start/RouteConfig.cs
+++ Examen.App/App_Start/RouteConfig.cs
@@ -13,11 +13,25 @@ namespace Examen.App
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
+            // Before all your routes
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                "Root",
+                "",
+                defaults: new { controller = "Home", action = "Index" });
+
+            // Your routes here
+
+            // After all your routes
+            routes.MapRoute(
+                "DeepLink",
+                "{*pathInfo}",
+                defaults: new { controller = "Home", action = "Index" });
         }
     }
 }
